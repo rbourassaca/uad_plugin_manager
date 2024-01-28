80895071db9431d80a750df6d8860bb7fb9c366c
Raphael Bourassa <bourassa.raphael@outlook.com>
2024-01-28 13:43:07 -0500

    Suppression de commentaires.

--- cmd/remove.go
+++ cmd/remove.go
@@ -13,17 +13,12 @@ var removeCmd = &cobra.Command{
 	Use:   "remove",
 	Short: "Remove plugins that aren't owned",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Load the UADSystemProfile
 		UADSystemProfile := file.Open(viper.Get("files.UADSystemProfile").(string))
-		// Load plugins to remove
 		toRemove := file.Find(UADSystemProfile, ": Demo not started", true)
-		// For each collections to remove
-		for i := 0; i<len(toRemove); i++ { 
-			// Creating a variable for the current collection
+		for i := 0; i<len(toRemove); i++ { // For each collections to remove
 			var collection []string
 			viper.UnmarshalKey(fmt.Sprintf("pluginDefinition.%s", toRemove[i]), &collection)
-			// For each plugins in the current collection
-			for x := 0; x < len(collection); x++ {
+			for x := 0; x < len(collection); x++ { // For each plugins in the current collection
 				plugin := collection[x]
 				fmt.Println(plugin)
 				//TODO: Move the current plugin
