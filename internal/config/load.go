35c613c2e4322906ace63fe22ee17e75756c4c62
Raphael Bourassa <bourassa.raphael@outlook.com>
2024-03-06 13:31:27 -0500

    Better handling of config files & allow loading them from the UserConfigDir.

--- internal/config/load.go
+++ internal/config/load.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"errors"
+	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -9,7 +12,7 @@ import (
 )
 
 type (
-	ConfigType struct {
+	Type struct {
 		Files            FilesType
 		PluginFormats    []PluginFormatType
 		PluginDefinition map[string][]string
@@ -26,27 +29,59 @@ type (
 	}
 )
 
-var Config ConfigType
+var Config Type
 var Appdata string
 var RemovedPluginDir string
 
 func Load() {
+	initEnv()
+	handleConfigFiles()
+	cleanPaths()
+}
+
+func initEnv() {
 	userConfigDir, err := os.UserConfigDir()
 	if err != nil {
 		os.Exit(1)
 	}
 	Appdata = filepath.Join(userConfigDir, "/UAD-Plugin-Manager")
 	RemovedPluginDir = filepath.Join(Appdata, "/Plugins")
-	os.Mkdir(Appdata, os.ModePerm)
-	viper.SetConfigFile(filepath.FromSlash("./config." + runtime.GOOS + ".yaml"))
-	viper.ReadInConfig()
-	viper.SetConfigFile(filepath.FromSlash("./pluginDefinition.yaml"))
-	viper.MergeInConfig()
-	viper.UnmarshalExact(&Config)
-	clean()
+	err = os.Mkdir(Appdata, os.ModePerm)
+	if err != nil {
+		if !errors.Is(err, fs.ErrExist) {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
 }
 
-func clean() {
+func handleConfigFiles() {
+	viper.AddConfigPath(Appdata)
+	viper.AddConfigPath(filepath.Clean("./"))
+	viper.SetConfigType("yaml")
+
+	viper.SetConfigName("config." + runtime.GOOS)
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	viper.SetConfigName("pluginDefinition")
+	err = viper.MergeInConfig()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = viper.UnmarshalExact(&Config)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func cleanPaths() {
 	Config.Files.UADSystemProfile = filepath.Clean(Config.Files.UADSystemProfile)
 	for i := 0; i < len(Config.PluginFormats); i++ {
 		Config.PluginFormats[i].Path = filepath.Clean(Config.PluginFormats[i].Path)
