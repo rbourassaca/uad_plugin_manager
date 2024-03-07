c772d48d434c4201cb88f0f117061e1fd9cecc9b
Raphael Bourassa <bourassa.raphael@outlook.com>
2024-03-07 11:23:58 -0500

    Added a timestamp folder to keep things organised.

--- internal/plugins/movePlugins.go
+++ internal/plugins/movePlugins.go
@@ -6,10 +6,13 @@ import (
 	"rbourassa/uadPluginManager/internal/config"
 	"rbourassa/uadPluginManager/internal/files"
 	"slices"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func MovePlugins(pluginsToMove []string) {
+	currentUnixTime := strconv.FormatInt(time.Now().Unix(), 10)
 	for i := 0; i < len(config.Config.PluginFormats); i++ {
 		glob, _ := filepath.Glob(config.Config.PluginFormats[i].Path + "/*" + config.Config.PluginFormats[i].Extension)
 		if len(glob) == 0 {
@@ -19,7 +22,7 @@ func MovePlugins(pluginsToMove []string) {
 			currentPath := GetPluginPaths(pluginsToMove[x], config.Config.PluginFormats[i].Extension, glob)
 			if slices.Contains(glob, currentPath) {
 				pluginLocation := strings.TrimPrefix(currentPath, config.Config.PluginFormats[i].Path)
-				newPath := filepath.Join(config.RemovedPluginDir, config.Config.PluginFormats[i].Name, pluginLocation)
+				newPath := filepath.Join(config.RemovedPluginDir, currentUnixTime, config.Config.PluginFormats[i].Name, pluginLocation)
 				err := files.Move(currentPath, newPath)
 				if err != nil {
 					fmt.Println(err.Error())
